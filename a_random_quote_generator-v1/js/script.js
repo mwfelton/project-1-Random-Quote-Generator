f10b140565d74df6ece98603b4507f4d8173b9b3
mwfelton <72751046+mwfelton@users.noreply.github.com>
2020-10-20 17:13:00 +0100

    Final version
    my finished project, left in commented out code at the bottom to show different approaches I tried along the way

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -13,64 +13,64 @@ project 1 - A Random Quote Generator
 
 const quoteArray =  [
 
-  // { 
-  //   quote: `In the midst of the common, the worldly, the mundane,
-  //   is the ineffable One. Knowing this,
-  //   one lives happily throughout all states.
-  //   This is the secret that only the sage knows
-  //   and enjoys and reveals to those whose time has come
-  //   to flower into divine understanding.`,
-  //   from: 'white fire: second edition',
-  //   guru: 'Mooji',
-  //   when: '2014'
-  // },
-  // { 
-  //   quote: `I am permanence and impermanence,
-  //   but I am neither, absolutely.
-  //   That which witnesses both,
-  //   but itself is uncaused and unseen,
-  //   That alone is. That I am.`,
-  //   from:'white fire: second edition',
-  //   guru: 'Mooji',
-  //   when: '2014'
-  // },
-  // { 
-  //   quote: `I teach you to be more conscious,
-  //   And love will come as you become more conscious: it is a guest that comes, 
-  //   that comes inevitably to those who are ready and prepared to receive it.`,
-  //   from: 'Satyam Shivam Sundaram: Truth Godliness Beauty',
-  //   guru: 'Osho',
-  //   when: ''
-  // },
-  // { 
-  //   quote: `A man of meditation comes to a point where there is no temptation left. Try to understand it. Temptation never comes from without: it is the repressed desire, repressed energy, repressed sex, repressed greed that creates temptation. 
-  //   Temptation comes from within you, it has nothing to do with the without.`,
-  //   from: 'Dang Dang Doko Dang , Talk #5',
-  //   guru: 'Osho',
-  //   when: ''
-  // },
-  // { 
-  //   quote: `Whatever was the highest, sweetest, or most wonderful emotion in your life, make that the baseline. 
-  //     Why come down from what you are capable of.`,
-  //   from: 'Sadhguru.org',
-  //   guru: 'Sadhguru Jaggi Vasudev',
-  //   when: '2020',
-  //   tags: 'Isha Foundation'
-  // },
-  // { 
-  //   quote: `Out of commitment, humans can move mountains. 
-  //     One shining example is Mahatma Gandhi, whose commitment moved millions of people across the world`,
-  //   from: 'Sadhguru.org',
-  //   guru: 'Sadhguru Jaggi Vasudev',
-  //   when: '2020'
-  // },
-  // { 
-  //   quote: `It takes a long time-many incarnations of right action, good company, help of the guru, self-awakening, wisdom, and meditation-for man to regain his soul consciousness of immortality. To reach this state of Self-realization, 
-  //     each man must practice meditation to transfer his consciousness from the limited body to the unlimited sphere of joy felt in meditation.`,
-  //   from: 'Paramahansa Yogananda - Self-Realization Fellowship',
-  //   guru: 'Paramahansa Yogananda',
-  //   when: ''
-  // },
+  { 
+    quote: `In the midst of the common, the worldly, the mundane,
+    is the ineffable One. Knowing this,
+    one lives happily throughout all states.
+    This is the secret that only the sage knows
+    and enjoys and reveals to those whose time has come
+    to flower into divine understanding.`,
+    from: 'white fire: second edition',
+    guru: 'Mooji',
+    when: '2014'
+  },
+  { 
+    quote: `I am permanence and impermanence,
+    but I am neither, absolutely.
+    That which witnesses both,
+    but itself is uncaused and unseen,
+    That alone is. That I am.`,
+    from:'white fire: second edition',
+    guru: 'Mooji',
+    when: '2014'
+  },
+  { 
+    quote: `I teach you to be more conscious,
+    And love will come as you become more conscious: it is a guest that comes, 
+    that comes inevitably to those who are ready and prepared to receive it.`,
+    from: 'Satyam Shivam Sundaram: Truth Godliness Beauty',
+    guru: 'Osho',
+    when: ''
+  },
+  { 
+    quote: `A man of meditation comes to a point where there is no temptation left. Try to understand it. Temptation never comes from without: it is the repressed desire, repressed energy, repressed sex, repressed greed that creates temptation. 
+    Temptation comes from within you, it has nothing to do with the without.`,
+    from: 'Dang Dang Doko Dang , Talk #5',
+    guru: 'Osho',
+    when: ''
+  },
+  { 
+    quote: `Whatever was the highest, sweetest, or most wonderful emotion in your life, make that the baseline. 
+      Why come down from what you are capable of.`,
+    from: 'Sadhguru.org',
+    guru: 'Sadhguru Jaggi Vasudev',
+    when: '2020 ',
+    tags: 'Isha Foundation'
+  },
+  { 
+    quote: `Out of commitment, humans can move mountains. 
+      One shining example is Mahatma Gandhi, whose commitment moved millions of people across the world`,
+    from: 'Sadhguru.org',
+    guru: 'Sadhguru Jaggi Vasudev',
+    when: '2020 '
+  },
+  { 
+    quote: `It takes a long time-many incarnations of right action, good company, help of the guru, self-awakening, wisdom, and meditation-for man to regain his soul consciousness of immortality. To reach this state of Self-realization, 
+      each man must practice meditation to transfer his consciousness from the limited body to the unlimited sphere of joy felt in meditation.`,
+    from: 'Paramahansa Yogananda - Self-Realization Fellowship',
+    guru: 'Paramahansa Yogananda',
+    when: ''
+  },
   { 
     quote: `Don't depend on death to liberate you from your imperfections. You are exactly the same after death as you were before. 
     Nothing changes; you only give up the body. If you are a thief or a liar or a cheater before death, you don't become an angel merely by dying. 
@@ -81,23 +81,8 @@ const quoteArray =  [
     guru: 'Paramahansa Yogananda',
     when: '',
     tags: 'Self-Realization Fellowship'
-  },
-  { 
-    quote: `example tester`,
-    from: 'miles',
-    guru: 'guru',
-    when: '',
-    tags: 'banana tags'
-  },
-  { 
-    quote: 'tester two',
-    from: 'miles',
-    guru: 'guru',
-    when: '',
-    tags: 'super tags'
   }
 ]
-
 
 /***
  * `getRandomQuote` function
@@ -111,6 +96,42 @@ function getRandomQuote(arr){
 /***
  * `printQuote` function
 ***/
+
+function printQuote(){
+  let num = getRandomQuote(quoteArray)
+  document.querySelector('.quote-box').innerHTML = `"<p class="quote">${quoteArray[num].quote}</p>" 
+  <p class="source">${quoteArray[num].guru}<span class="citation">${quoteArray[num].from}</span><span class="year">${quoteArray[num].when}</span></p>`;
+
+  if (quoteArray[num].tags){
+    document.querySelector('.year').innerText += `${quoteArray[num].tags}`;
+    }
+}
+
+function makeColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let rgbGo = `rgb( ${red}, ${green}, ${blue} )`;
+  return document.querySelector('.container').style.backgroundColor = rgbGo;
+}
+
+setInterval(printQuote, 2000);
+setInterval(makeColor, 2000);
+// setInterval(extraProp, 2000);
+
+/* /* /* /***
+* click event listener for the print quote button
+* DO NOT CHANGE THE CODE BELOW!!
+***/ 
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", makeColor, false);
+
+
+
+
+
+// Other experiments along the way
 
 // function printQuote(){
 //     let num = getRandomQuote(quoteArray)
@@ -138,18 +159,6 @@ function getRandomQuote(arr){
 //   }
 // }
 
-function printQuote(){
-  let num = getRandomQuote(quoteArray)
-  document.querySelector('.quote-box').innerHTML = `"<p class="quote">${quoteArray[num].quote}</p>" 
-  <p class="source">${quoteArray[num].guru}<span class="citation">${quoteArray[num].from}</span><span class="year">${quoteArray[num].when}</span></p>`;
-
-  if (quoteArray[num].tags){
-    document.querySelector('.year').innerText += `${quoteArray[num].tags}`;
-    console.log(quoteArray[num]);
-    }
-}
-
-
 // if (Object.keys(quoteArray[random].length > 4)) {
 //     document.querySelector('.quote-box').insertAdjacentHTML = `"<p class="extra">${quoteArray[num].tags}</p>"`
 // }
@@ -171,23 +180,3 @@ function printQuote(){
 //   console.log(rgbGo);
 //   return document.querySelector('.container').style.backgroundColor = rgbGo;
 // }
-
-function makeColor() {
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
-    let rgbGo = `rgb( ${red}, ${green}, ${blue} )`;
-    return document.querySelector('.container').style.backgroundColor = rgbGo;
-}
-
-setInterval(printQuote, 2000);
-setInterval(makeColor, 2000);
-// setInterval(extraProp, 2000);
-
-/* /* /* /***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/ 
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
-// document.getElementById('load-quote').addEventListener("click", makeColor, false);
